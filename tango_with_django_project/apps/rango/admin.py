83a7ff38e1e499f94fadf7b05e0745dcdcd2bb6f
Htet Naing Aung <justin.aung19@gmail.com>
2017-01-16 21:37:45 +0700

    update ‘Page’ for admin to show title, category and url

--- tango_with_django_project/apps/rango/admin.py
+++ tango_with_django_project/apps/rango/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from apps.rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
