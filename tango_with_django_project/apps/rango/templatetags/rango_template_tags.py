310a28f73648a7e419d584577021b7bfe4d70a87
Htet Naing Aung <justin.aung19@gmail.com>
2017-02-11 22:52:08 +0700

    Use custom template tags to add browsable categories in the sidebar

---
+++ tango_with_django_project/apps/rango/templatetags/rango_template_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from apps.rango.models import Category
+
+register = template.Library()
+
+
+@register.inclusion_tag('rango/cats.html')
+def get_category_list(category=None):
+    return {'categories': Category.objects.all(),
+            'act_cat': category}
