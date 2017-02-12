4a5c02af12b581c22c2dd2eedd8d0fb8d67c1689
Htet Naing Aung <justin.aung19@gmail.com>
2017-02-12 14:35:13 +0700

    Add User model

--- tango_with_django_project/apps/rango/models.py
+++ tango_with_django_project/apps/rango/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use Python 2.7.x, define __unicode__ too!
+    def __str__(self):
+        return self.user.username
 
 
 class Category(models.Model):
